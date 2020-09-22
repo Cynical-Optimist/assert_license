97bc33c64ef538f901bce6a755f51b071f01bf9d
Douglas Winship <douglas.winship@codethink.co.uk>
2020-09-22 19:27:12 +0100

    implement blacklist

--- plugins/elements/assert_license.py
+++ plugins/elements/assert_license.py
@@ -26,11 +26,27 @@ import os.path
 from buildstream import Element, ElementError, Scope
 
 
+def license_matches(blacklist_entry, license_string):
+    "Test whether a given line of license info violates a given line from the blacklist"
+    # This is a provisional version of the function, and will be updated soon.
+    # For now, a 'match' is true if either line contains the other line.
+    # But other options should be considered (such as regEx matching)
+
+    # Also: the function should be expanded to handle license strings containing
+    # "OR" and "AND" statements.
+
+    if blacklist_entry in license_string:
+        return True
+    if license_string in blacklist_entry:
+        return True
+    return False
+
 
 class AssertLicenseElement(Element):
     def configure(self, node):
-        self.node_validate(node, ["dependency_scope", "path"])
+        self.node_validate(node, ["path", "blacklist", "dependency_scope"])
         self.path = self.node_subst_member(node, "path")
+        self.blacklist = self.node_subst_list(node, "blacklist")
 
         dependency_scope = self.node_subst_member(node, "dependency_scope").lower()
         if dependency_scope == "run":
@@ -55,6 +71,7 @@ class AssertLicenseElement(Element):
         return {
             "scope": str(self.dep_scope),
             "path": self.path,
+            "blacklist": self.blacklist,
         }
 
     def configure_sandbox(self, sandbox):
@@ -62,19 +79,6 @@ class AssertLicenseElement(Element):
 
     def stage(self, sandbox):
         pass
-
-    def assemble(self, sandbox):
-        output_dict = {}
-        for dep in self.get_deps():
-            output_dict[dep.name] = dep.get_public_data("licenses")["license-strings"]
-
-        basedir = sandbox.get_directory()
-        path = os.path.join(basedir, self.path)
-        with open(path, mode="w") as outfile:
-            json.dump(output_dict, outfile, indent=2)
-            outfile.write("\n")
-
-        return os.path.sep
 
     def get_deps(self):
         already_visited = []
@@ -89,6 +93,36 @@ class AssertLicenseElement(Element):
                     if dep not in already_visited:
                         already_visited.append(dep)
                         yield dep
+
+    def assemble(self, sandbox):
+        output_dict = {}
+        blacklist_violations = False
+        for dep in self.get_deps():
+            license_strings = dep.get_public_data("licenses")["license-strings"]
+            output_dict[dep.name] = license_strings
+            for blacklist_string in self.blacklist:
+                for license_string in license_strings:
+                    if license_matches(blacklist_string, license_string):
+                        self.warn(
+                            "Blacklist violation",
+                            detail=(
+                                f"In Element: {dep.name},"
+                                f' license string "{license_string}" conflicts with'
+                                f' blacklist entry "{blacklist_string}"'
+                            ),
+                        )
+                        blacklist_violations = True
+
+        if blacklist_violations:
+            raise ElementError("assert_license element detected a blacklist violation")
+
+        basedir = sandbox.get_directory()
+        path = os.path.join(basedir, self.path)
+        with open(path, mode="w") as outfile:
+            json.dump(output_dict, outfile, indent=2)
+            outfile.write("\n")
+
+        return os.path.sep
 
 
 def setup():
